23bbf119157699164aaa9b369459402c563e9a31
Mukesh-creator <72543433+Mukesh-creator@users.noreply.github.com>
2021-06-13 12:07:35 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+import os
+
+
+def read_requirements():
+    """Parse requirements from requirements.txt."""
+    requirements_path = os.path.join('.', 'requirements.txt')
+    with open(requirements_path, 'r') as f:
+        requirements = [line.rstrip() for line in f]
+    return requirements
+
+
+setup(
+    name                = 'mediacon-py',
+    description         = 'Find usernames in social networks.',
+    author              = 'Mukesh',
+    url                 = 'https://github.com/decoxviii/userrecon-py',
+    packages            = find_packages(),
+    scripts             = ['bin/mediacon-py'],
+    install_requires    = read_requirements(),
+    classifiers = [
+        'Programming Language :: Python3',
+        'Environment :: Console',
+    ],
+)
+
